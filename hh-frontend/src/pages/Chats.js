722adbcdffc7ba8654ba9914395ac0acfa225c58
Nick D'Esposito <Nicholas D'Esposito>
2022-12-12 10:41:22 -0500

    needed for new frontend

--- hh-frontend/src/pages/Chats.js
+++ hh-frontend/src/pages/Chats.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useContext, useState } from "react";
 import { CurrentUser } from "../context/CurrentUser";
 import { Card, Container } from "react-bootstrap";
+import "react-router-dom";
 
 function Chats() {
     const currentUser = useContext(CurrentUser);
     const [chats, setChats] = useState([]);
-    let children;
+    const [children, setChildren] = useState();
+    let resName;
 
     useEffect(() => {
         const fetchChats = async () => {
@@ -24,28 +26,44 @@ function Chats() {
     }, []);
     console.log(chats);
 
+    async function fetchName(id) {
+        const response = await fetch(
+            `http://localhost:5050/memberAccounts/${id}`,
+            {
+                method: "GET",
+            }
+        );
+        const data = await response.json();
+        const name = JSON.stringify(data.name);
+        console.log(data.name);
+        return name;
+    }
+
     if (chats === []) {
         console.log("here 29");
         return <h1>No Chats</h1>;
     } else {
         console.log("hello 32");
         // console.log(chats[0].bothParties);
-        children = chats.map((chat, i) => {
-            return (
-                <div key={i}>
-                    <Container>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>{chat.bothParties[0]}</Card.Title>
-                                <Card.Text>
-                                    {chat.messages[0].message}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Container>
-                </div>
-            );
-        });
+        setChildren(
+            chats.map(async (chat, i) => {
+                // const id =(currentUser._id == chat.bothParties[0]) ? chat.bothParties[1]: chat.bothParties[0];
+                // const name = fetchName(id);
+                // resName = name;
+                return (
+                    <div key={i}>
+                        <Container>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>hello</Card.Title>
+                                    <Card.Text>Hi</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Container>
+                    </div>
+                );
+            })
+        );
     }
 
     return (
